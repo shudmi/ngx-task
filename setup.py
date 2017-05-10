58dd2d188aab1fbf30ff843307eecf5ca685527c
shudmi <dmitri.shulyak@gmail.com>
2017-05-10 09:54:08 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ngx-task',
+    version='0.1',
+    description='Testimonial for candidates to show up their code-foo',
+    author='Dmitry Shulyak',
+    author_email='dmitri.shulyak@gmail.com',
+    url='https://github.com/shudmi/ngx-task',
+    classifiers=[
+        'License :: Apache License 2.0',
+        'Programming Language :: Python',
+        'Programming Language :: Python 3',
+        'Programming Language :: Python 3.4',
+    ],
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    install_requires=[],
+    entry_points="""
+        [console_scripts]
+        ngx_generate=ngx_task.cli.generate_data
+        ngx_process=ngx_task.cli.process_data
+    """
+)
